0b85aaf177954214bbd943fc019d9edbfd1526f3
prajapati-satyam <satyamprajapati1979@gmail.com>
2025-10-21 12:12:21 +0530

    -- dockerise app

--- connection/redis-connect.js
+++ connection/redis-connect.js
@@ -1,3 +1,6 @@
 import {Redis} from "ioredis"
 
-export const redisConnection = new Redis({maxRetriesPerRequest: null});
+export const redisConnection = new Redis(
+    {maxRetriesPerRequest: null,
+        host: "valkey",
+        port: 6379});
