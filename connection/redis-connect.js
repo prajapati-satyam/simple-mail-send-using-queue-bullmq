60eed80f4cdf823d979994b91ab8cca58f6dd279
prajapati-satyam <satyamprajapati1979@gmail.com>
2026-03-31 11:14:20 +0530

    -- trim connetion file

--- connection/redis-connect.js
+++ connection/redis-connect.js
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config()
 import { RedisConnection } from "bullmq";
-import {Redis} from "ioredis"
+import { Redis } from "ioredis"
 
 export const redisConnection = new Redis(process.env.REDIS_URL,
-    {maxRetriesPerRequest: null,
+    {
+        maxRetriesPerRequest: null,
         // host: "",
         port: 6379,
     });
